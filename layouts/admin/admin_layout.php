9815d62a79dc92df3f5233ed1b79bb3bd997dc01
andypratama3 <andypratama1211@gmail.com>
2023-12-09 20:43:39 +0800

    updated error bukutamu

--- layouts/admin/admin_layout.php
+++ layouts/admin/admin_layout.php
@@ -4,7 +4,7 @@
     <i class='bx bx-menu' id="menu-icon"></i>
     <nav class="navbar">
         <a href="../../index.php">Home</a> 
-        <a href="../admin/bukutamu/index.php">Buku Tamu</a>
+        <a href="../../admin/bukutamu/index.php">Buku Tamu</a>
         <a href="../../admin/experience/index.php">Experience</a>
         <a href="../../admin/user/index.php">User</a>
         <?php if(checkLogin()) : ?>
